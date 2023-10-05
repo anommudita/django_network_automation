d7bd7febd4070b0c42c34a135139ce9e5518f1e4
anommudita <gusmangananda@gmail.com>
2023-10-05 11:05:53 +0800

    api proxmox

--- network_automation/views.py
+++ network_automation/views.py
@@ -9,12 +9,18 @@ import time
 import json
 
 
+# import request dan jsonRespon untuk API promoxer
+import requests
+from django.http import JsonResponse
+
+
+
 # message flash django
 from django.contrib import messages
 
 
 # api proxmox
-# from proxmoxer import ProxmoxAPI
+from proxmoxer import ProxmoxAPI
 
 import paramiko
 
@@ -34,30 +40,67 @@ def home(request):
 #  halaman user
 def  user(request):
 
-    host = "192.168.18.125"
-    username = "root"
-    password = "123123123"
+    # host = "192.168.18.125"
+    # username = "root"
+    # password = "123123123"
     
-    client = paramiko.client.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username=username, password=password)
-    _stdin, _stdout,_stderr = client.exec_command("pveum user list --output-format json-pretty")
+    # client = paramiko.client.SSHClient()
+    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # client.connect(host, username=username, password=password)
+    # _stdin, _stdout,_stderr = client.exec_command("pveum user list --output-format json-pretty")
 
-    # print(_stdout.read().decode())
-    test_json = _stdout.read().decode()
-    client.close()
+    # # print(_stdout.read().decode())
+    # test_json = _stdout.read().decode()
+    # client.close()
 
-    test = json.loads(test_json)
+    # test = json.loads(test_json)
 
-    users = User.objects.all()
-    
-    context = {
-        'title': 'User',
-        'active_user': 'active',
-        'users': users,
-        'test' : test
+    # Autentikasi Proxmox API
+    proxmox_url = 'https://192.168.1.15/api2/json/access/users'
+    proxmox_user = 'root@pam'
+    proxmox_password = '123123123'
+
+    # Header dengan informasi otentiskasi
+    headers = {
+        'Content-Type': 'application/json',
     }
-    return render(request, 'base/user.html', context )
+
+
+    # Request GET Ke API Proxmox
+    try:
+        response = requests.get(
+            proxmox_url,
+            headers=headers,
+            auth=(proxmox_user, proxmox_password),
+            verify=False  # Ganti ke True jika Anda ingin mengaktifkan verifikasi SSL
+        )
+
+        # Periksa status kode respons
+        if response.status_code == 200:
+            data = response.json()
+            # return JsonResponse(data, safe=False)
+            proxmox_users = data.get('data', [])
+            return render(request, 'base/user.html', {'proxmox_users': proxmox_users})
+        else:
+            return JsonResponse({'error': 'Failed to retrieve Proxmox users.'}, status=500)
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({'error': f'Failed to connect to Proxmox API: {str(e)}'}, status=500)
+
+
+
+    # prox = ProxmoxAPI('10.10.10.10', user='root@pam', password='password', verify_ssl=False)
+
+    # test = ''
+
+    # users = User.objects.all()
+    
+    # context = {
+    #     'title': 'User',
+    #     'active_user': 'active',
+    #     'users': users,
+    #     'test' : test
+    # }
+    # return render(request, 'base/user.html', context )
 
 # add user
 def  addUser(request):
