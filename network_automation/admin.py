871a4a5fc327615b92624c19631d478945491dcf
anommudita <gusmangananda@gmail.com>
2023-10-18 15:28:46 +0800

    done fitur config- admin

--- network_automation/admin.py
+++ network_automation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Server
+from .models import Server, UserProfile
 
 admin.site.register(Server)
+admin.site.register(UserProfile)
